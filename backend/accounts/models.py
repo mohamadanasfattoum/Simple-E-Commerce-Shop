1fe9ab90a553afc8f49cc536ef52ba69c8bd6348
mohamadanasfattoum <samaanas30@gmail.com>
2025-02-09 14:25:35 +0100

    created useraccount model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserAccount(models.Model):
+    user = models.OneToOneField(User, related_name='Accounts', on_delete=models.CASCADE)
+    note  = models.TextField(max_length=100, blank=True, null=True)
