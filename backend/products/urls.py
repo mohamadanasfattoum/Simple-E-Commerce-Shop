e90a19486c43c28bd5fe780d19444db164c4a886
mohamadanasfattoum <samaanas30@gmail.com>
2025-02-18 22:38:40 +0100

    try to shoe product detail in detail page

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -5,10 +5,10 @@ from .api import ProductsApi, BrandsApi, ProductDetailApi, BrandDetailApi
 
 urlpatterns = [
     path("products/", ProductsApi.as_view(), name="products_api"),
-    path("products/<int:pk>", ProductDetailApi().as_view(), name="product_detail_api"),
+    path("products/<int:pk>/", ProductDetailApi().as_view(), name="product_detail_api"),
 
     path("brands/", BrandsApi.as_view(), name="brands_api"),
-    path("brands/<int:pk>", BrandDetailApi.as_view(), name="brand_detail_api"),
+    path("brands/<int:pk>/", BrandDetailApi.as_view(), name="brand_detail_api"),
 
 
 ]
